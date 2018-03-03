6012e014f3746703a6c345c2cd8a7de720572d28
kharazzoro <kha.1991.14.2017@gmail.com>
2018-03-03 15:53:12 +0000

    initalize coffe mangment redux app

--- redux-apps/src/App.js
+++ redux-apps/src/App.js
@@ -4,6 +4,10 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import ReduxContainer from './container/ReduxContainer';
 import ReduxContainerCalculator from './container/ReduxContainerCalculator';
+import ReduxCoffeMangment from './container/coffeMangment/ReduxCoffeMangment';
+
+import CoffeSetPricess from './container/coffeMangment/CoffeSetPricess';
+import CoffeOrder from './container/coffeMangment/CoffeOrder';
 
 import Menu from './component/menu'
 class App extends Component {
@@ -15,6 +19,10 @@ class App extends Component {
           <Route exact path="/" component={Menu} />
           <Route path="/redux-counter" component={ReduxContainer} />
           <Route path="/redux-calculator" component={ReduxContainerCalculator} />
+          <Route path="/redux-coffe-mangment" component={ReduxCoffeMangment} />
+          <Route path="/redux-coffe-mangment/set-price" component={CoffeSetPricess} />
+          <Route path="/redux-coffe-mangment/newOrder" component={CoffeOrder} />
+          
         </div>
       </Router>
     );
---
+++ redux-apps/src/container/coffeMangment/CoffeSetPricess.js
@@ -0,0 +1,142 @@
+import React, { Component } from 'react';
+import '../css/coffeMangment.css'
+
+class CoffeSetPricess extends Component {
+    render() {
+        return (
+            <div class="coffe-pricess">
+                <p>Coffe Set Pricess</p>
+
+
+                <h1>Coffe List Pricess</h1>
+
+                <div class="coffe-items">
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                </div>
+
+
+
+                
+                <h1>Cake List Pricess</h1>
+
+                <div class="coffe-items">
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                </div>
+
+
+
+                
+                <h1>Drink List Pricess</h1>
+
+                <div class="coffe-items">
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                    <div>
+                        <p>Lettee</p>
+                        <input type="number" placeholder="£"/>
+                    </div>
+
+                </div>
+
+            </div>)
+    }
+
+}
+
+export default CoffeSetPricess;
